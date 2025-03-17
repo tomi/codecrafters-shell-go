28ace9b66f02d851039863b4b714e0e242ca1916
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-03-17 09:56:12 +0200

    implement exit builtin

--- app/main.go
+++ app/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -14,17 +15,22 @@ func main() {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		command := promptCommand()
+		switch command[0] {
+		case "exit":
+			os.Exit(0)
 
-		fmt.Println(command + ": command not found")
+		default:
+			fmt.Println(command[0] + ": command not found")
+		}
 	}
 }
 
-func promptCommand() string {
+func promptCommand() []string {
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error reading command: %v\n", err)
 		os.Exit(1)
 	}
 
-	return command[:len(command)-1]
+	return strings.Split(command[:len(command)-1], " ")
 }
