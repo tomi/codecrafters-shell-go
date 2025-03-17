6630a987dc49aa7b838d6d97808ab056b1a735c6
Tomi Turtiainen <10324676+tomi@users.noreply.github.com>
2025-03-17 10:03:13 +0200

    refactor: Extract builtins to own functions

--- app/main.go
+++ app/main.go
@@ -7,33 +7,48 @@ import (
 	"strings"
 )
 
-// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
+type Command struct {
+	Name string
+	Args []string
+}
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		command := promptCommand()
-		switch command[0] {
+		switch command.Name {
 		case "exit":
-			os.Exit(0)
+			exitCommand(command)
 
 		case "echo":
-			fmt.Println(strings.Join(command[1:], " "))
+			echoCommand(command)
 
 		default:
-			fmt.Println(command[0] + ": command not found")
+			fmt.Println(command.Name + ": command not found")
 		}
 	}
 }
 
-func promptCommand() []string {
+func promptCommand() Command {
 	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error reading command: %v\n", err)
 		os.Exit(1)
 	}
 
-	return strings.Split(command[:len(command)-1], " ")
+	parts := strings.Split(command[:len(command)-1], " ")
+
+	return Command{
+		Name: parts[0],
+		Args: parts[1:],
+	}
+}
+
+func exitCommand(_ Command) {
+	os.Exit(0)
+}
+
+func echoCommand(command Command) {
+	fmt.Println(strings.Join(command.Args, " "))
 }
